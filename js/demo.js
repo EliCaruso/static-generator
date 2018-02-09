653429d2e20a2066f579fd56040c7760f817fbe0
studente <studente@liceospalla.local>
2018-02-09 10:47:55 +0100

    ce la potrei quasi fare

--- js/demo.js
+++ js/demo.js
@@ -14,5 +14,15 @@ const articles = [
     lastUpdateDate: 'fra 2 ore'
   }
 ];
-const html = ejs.render(fs.readFileSync('./index.html', { encoding: 'utf8' }), { articles: articles });
+const images = [
+  {
+    image: 'http://tuttoin1.it/wp-content/uploads/2015/10/atomo-752x440.jpg',
+    text: 'Ancora più scemo chi continua a leggere',
+  },
+  {
+    image: 'http://tuttoin1.it/wp-content/uploads/2015/10/atomo-752x440.jpg',
+    text: 'Ancora più gay chi continua a leggere',
+  }
+];
+const html = ejs.render(fs.readFileSync('./index.html', { encoding: 'utf8' }), { articles: articles, images: images });
 fs.writeFileSync('./index2.html', html, { encoding: 'utf8' });
